f3f0943dff5f902b362982fa34166885fb2e8ca1
Neel Shettigar <neelshettigar@gmail.com>
2020-08-28 18:08:14 -0400

    Add files via upload

---
+++ Projects/BookRec/prjBookRec/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmBookRec
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblReader = New System.Windows.Forms.Label()
+        Me.lblAuthor = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.cmdDone = New System.Windows.Forms.Button()
+        Me.cmdNext = New System.Windows.Forms.Button()
+        Me.RB5 = New System.Windows.Forms.RadioButton()
+        Me.RB3 = New System.Windows.Forms.RadioButton()
+        Me.RB1 = New System.Windows.Forms.RadioButton()
+        Me.RB0 = New System.Windows.Forms.RadioButton()
+        Me.RBn3 = New System.Windows.Forms.RadioButton()
+        Me.RBn5 = New System.Windows.Forms.RadioButton()
+        Me.txtRec = New System.Windows.Forms.TextBox()
+        Me.cmbMethods = New System.Windows.Forms.ComboBox()
+        Me.lstReaders = New System.Windows.Forms.ListBox()
+        Me.cmbEdit = New System.Windows.Forms.ComboBox()
+        Me.lstBooks = New System.Windows.Forms.ListBox()
+        Me.picBookCover = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.picBookCover, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblReader)
+        Me.GroupBox2.Controls.Add(Me.lblAuthor)
+        Me.GroupBox2.Controls.Add(Me.lblTitle)
+        Me.GroupBox2.Controls.Add(Me.cmdDone)
+        Me.GroupBox2.Controls.Add(Me.cmdNext)
+        Me.GroupBox2.Controls.Add(Me.RB5)
+        Me.GroupBox2.Controls.Add(Me.RB3)
+        Me.GroupBox2.Controls.Add(Me.RB1)
+        Me.GroupBox2.Controls.Add(Me.RB0)
+        Me.GroupBox2.Controls.Add(Me.RBn3)
+        Me.GroupBox2.Controls.Add(Me.RBn5)
+        Me.GroupBox2.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 190)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(418, 90)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Rating Books"
+        '
+        'lblReader
+        '
+        Me.lblReader.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblReader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblReader.Location = New System.Drawing.Point(6, 22)
+        Me.lblReader.Name = "lblReader"
+        Me.lblReader.Size = New System.Drawing.Size(130, 27)
+        Me.lblReader.TabIndex = 19
+        Me.lblReader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAuthor
+        '
+        Me.lblAuthor.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblAuthor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAuthor.Location = New System.Drawing.Point(283, 22)
+        Me.lblAuthor.Name = "lblAuthor"
+        Me.lblAuthor.Size = New System.Drawing.Size(130, 27)
+        Me.lblAuthor.TabIndex = 18
+        Me.lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTitle
+        '
+        Me.lblTitle.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTitle.Location = New System.Drawing.Point(145, 22)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(130, 27)
+        Me.lblTitle.TabIndex = 17
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'cmdDone
+        '
+        Me.cmdDone.Location = New System.Drawing.Point(338, 56)
+        Me.cmdDone.Name = "cmdDone"
+        Me.cmdDone.Size = New System.Drawing.Size(73, 25)
+        Me.cmdDone.TabIndex = 16
+        Me.cmdDone.Text = "Done"
+        Me.cmdDone.UseVisualStyleBackColor = True
+        '
+        'cmdNext
+        '
+        Me.cmdNext.Location = New System.Drawing.Point(257, 56)
+        Me.cmdNext.Name = "cmdNext"
+        Me.cmdNext.Size = New System.Drawing.Size(73, 25)
+        Me.cmdNext.TabIndex = 15
+        Me.cmdNext.Text = "Next"
+        Me.cmdNext.UseVisualStyleBackColor = True
+        '
+        'RB5
+        '
+        Me.RB5.AutoSize = True
+        Me.RB5.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB5.Location = New System.Drawing.Point(209, 58)
+        Me.RB5.Name = "RB5"
+        Me.RB5.Size = New System.Drawing.Size(35, 21)
+        Me.RB5.TabIndex = 14
+        Me.RB5.Text = "5"
+        Me.RB5.UseVisualStyleBackColor = True
+        '
+        'RB3
+        '
+        Me.RB3.AutoSize = True
+        Me.RB3.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB3.Location = New System.Drawing.Point(169, 58)
+        Me.RB3.Name = "RB3"
+        Me.RB3.Size = New System.Drawing.Size(35, 21)
+        Me.RB3.TabIndex = 13
+        Me.RB3.Text = "3"
+        Me.RB3.UseVisualStyleBackColor = True
+        '
+        'RB1
+        '
+        Me.RB1.AutoSize = True
+        Me.RB1.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB1.Location = New System.Drawing.Point(129, 58)
+        Me.RB1.Name = "RB1"
+        Me.RB1.Size = New System.Drawing.Size(35, 21)
+        Me.RB1.TabIndex = 12
+        Me.RB1.Text = "1"
+        Me.RB1.UseVisualStyleBackColor = True
+        '
+        'RB0
+        '
+        Me.RB0.AutoSize = True
+        Me.RB0.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RB0.Location = New System.Drawing.Point(89, 58)
+        Me.RB0.Name = "RB0"
+        Me.RB0.Size = New System.Drawing.Size(35, 21)
+        Me.RB0.TabIndex = 11
+        Me.RB0.Text = "0"
+        Me.RB0.UseVisualStyleBackColor = True
+        '
+        'RBn3
+        '
+        Me.RBn3.AutoSize = True
+        Me.RBn3.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBn3.Location = New System.Drawing.Point(49, 58)
+        Me.RBn3.Name = "RBn3"
+        Me.RBn3.Size = New System.Drawing.Size(40, 21)
+        Me.RBn3.TabIndex = 10
+        Me.RBn3.Text = "-3"
+        Me.RBn3.UseVisualStyleBackColor = True
+        '
+        'RBn5
+        '
+        Me.RBn5.AutoSize = True
+        Me.RBn5.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBn5.Location = New System.Drawing.Point(9, 58)
+        Me.RBn5.Name = "RBn5"
+        Me.RBn5.Size = New System.Drawing.Size(40, 21)
+        Me.RBn5.TabIndex = 9
+        Me.RBn5.Text = "-5"
+        Me.RBn5.UseVisualStyleBackColor = True
+        '
+        'txtRec
+        '
+        Me.txtRec.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txtRec.Location = New System.Drawing.Point(12, 12)
+        Me.txtRec.Multiline = True
+        Me.txtRec.Name = "txtRec"
+        Me.txtRec.ReadOnly = True
+        Me.txtRec.Size = New System.Drawing.Size(275, 172)
+        Me.txtRec.TabIndex = 10
+        '
+        'cmbMethods
+        '
+        Me.cmbMethods.FormattingEnabled = True
+        Me.cmbMethods.Items.AddRange(New Object() {"Method A", "Method B", "Method C"})
+        Me.cmbMethods.Location = New System.Drawing.Point(524, 257)
+        Me.cmbMethods.Name = "cmbMethods"
+        Me.cmbMethods.Size = New System.Drawing.Size(82, 21)
+        Me.cmbMethods.TabIndex = 11
+        Me.cmbMethods.Text = "Recommend"
+        '
+        'lstReaders
+        '
+        Me.lstReaders.FormattingEnabled = True
+        Me.lstReaders.Location = New System.Drawing.Point(436, 103)
+        Me.lstReaders.Name = "lstReaders"
+        Me.lstReaders.Size = New System.Drawing.Size(170, 147)
+        Me.lstReaders.TabIndex = 12
+        '
+        'cmbEdit
+        '
+        Me.cmbEdit.FormattingEnabled = True
+        Me.cmbEdit.Items.AddRange(New Object() {"Add", "Change", "Delete"})
+        Me.cmbEdit.Location = New System.Drawing.Point(436, 257)
+        Me.cmbEdit.Name = "cmbEdit"
+        Me.cmbEdit.Size = New System.Drawing.Size(82, 21)
+        Me.cmbEdit.TabIndex = 13
+        Me.cmbEdit.Text = "Edit"
+        '
+        'lstBooks
+        '
+        Me.lstBooks.FormattingEnabled = True
+        Me.lstBooks.Location = New System.Drawing.Point(436, 12)
+        Me.lstBooks.Name = "lstBooks"
+        Me.lstBooks.Size = New System.Drawing.Size(170, 82)
+        Me.lstBooks.TabIndex = 14
+        '
+        'picBookCover
+        '
+        Me.picBookCover.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBookCover.Location = New System.Drawing.Point(295, 12)
+        Me.picBookCover.Name = "picBookCover"
+        Me.picBookCover.Size = New System.Drawing.Size(130, 172)
+        Me.picBookCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBookCover.TabIndex = 15
+        Me.picBookCover.TabStop = False
+        '
+        'frmBookRec
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 288)
+        Me.Controls.Add(Me.picBookCover)
+        Me.Controls.Add(Me.lstBooks)
+        Me.Controls.Add(Me.cmbEdit)
+        Me.Controls.Add(Me.lstReaders)
+        Me.Controls.Add(Me.cmbMethods)
+        Me.Controls.Add(Me.txtRec)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Name = "frmBookRec"
+        Me.Text = "BookRec"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.picBookCover, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RB5 As RadioButton
+    Friend WithEvents RB3 As RadioButton
+    Friend WithEvents RB1 As RadioButton
+    Friend WithEvents RB0 As RadioButton
+    Friend WithEvents RBn3 As RadioButton
+    Friend WithEvents RBn5 As RadioButton
+    Friend WithEvents cmdDone As Button
+    Friend WithEvents cmdNext As Button
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtRec As TextBox
+    Friend WithEvents cmbMethods As ComboBox
+    Friend WithEvents lstReaders As ListBox
+    Friend WithEvents cmbEdit As ComboBox
+    Friend WithEvents lblReader As Label
+    Friend WithEvents lstBooks As ListBox
+    Friend WithEvents picBookCover As PictureBox
+End Class
---
+++ Projects/BookRec/prjBookRec/Form1.vb
@@ -0,0 +1,319 @@
+﻿Imports System.ComponentModel
+Public Class frmBookRec
+    Dim counter As Integer
+    Dim TotalReaders As Integer
+    Structure Reader
+        Dim Name As String
+        Dim arrRatings() As Integer
+    End Structure
+    Structure Readerlist
+        Dim arrReaders() As Reader
+        Dim NumNew As Integer
+        Sub Create()
+            frmBookRec.counter = 0
+            Dim path As String = "D:\CP\HCP2\Projects\BookRec\readerratings.txt"
+            FileOpen(2, path, OpenMode.Input)
+            Do While Not EOF(2)
+                Dim test As String = LineInput(2)
+                frmBookRec.counter = frmBookRec.counter + 1
+            Loop
+            frmBookRec.counter = frmBookRec.counter / 2
+            FileClose(2)
+            Dim i As Integer = 1
+            FileOpen(2, path, OpenMode.Input)
+            ReDim arrReaders(frmBookRec.counter)
+            Do While Not EOF(2)
+                ReDim arrReaders(i).arrRatings(55)
+                Dim Name As String = LineInput(2)
+                arrReaders(i).Name = Name
+                Dim arrSplit() As String
+                Dim strline As String = LineInput(2)
+                arrSplit = strline.Split("  ")
+                For x As Integer = 1 To 55
+                    arrReaders(i).arrRatings(x) = Val(arrSplit(x - 1))
+                Next
+                i += 1
+            Loop
+            FileClose(2)
+            NumNew = 0
+        End Sub
+        Sub Add(Name As String)
+            NumNew = NumNew + 1
+            ReDim Preserve arrReaders(86 + NumNew)
+            arrReaders(86 + NumNew).Name = Name
+            frmBookRec.TotalReaders = 86 + NumNew
+        End Sub
+    End Structure
+    Structure Book
+        Dim Title As String, Author As String
+    End Structure
+    Structure Booklist
+        Dim arrBooks() As Book
+        Sub Create()
+            ReDim arrBooks(55)
+            Dim path As String = "D:\CP\HCP2\Projects\BookRec\books.txt"
+            FileOpen(1, path, OpenMode.Input)
+            Dim i As Integer = 1
+            Do While Not EOF(1)
+                Dim strline As String = LineInput(1)
+                Dim midcheck As Boolean = False
+                Dim position As Integer
+                Dim x As Integer = 1
+                Do While midcheck = False
+                    If Mid(strline, x, 1) = "," Then
+                        position = x
+                        midcheck = True
+                    End If
+                    x = x + 1
+                Loop
+                arrBooks(i).Author = (Mid(strline, 1, position - 1))
+                arrBooks(i).Title = (Mid(strline, position + 1, (Len(strline) - position)))
+                i += 1
+            Loop
+            FileClose(1)
+        End Sub
+    End Structure
+    Dim Books As Booklist
+    Dim Readers As Readerlist
+    Dim arrButtons(6) As RadioButton
+    Sub SetControlArrays()
+        arrButtons(1) = RBn5
+        arrButtons(2) = RBn3
+        arrButtons(3) = RB0
+        arrButtons(4) = RB1
+        arrButtons(5) = RB3
+        arrButtons(6) = RB5
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Readers.Create()
+        Books.Create()
+        SetControlArrays()
+        ReaderPos = 0
+        BookPos = 0
+        For i = 1 To counter
+            lstReaders.Items.Add(Readers.arrReaders(i).Name)
+        Next
+        TotalReaders = counter
+    End Sub
+    Structure BookRec
+        Dim arrSimilarityScore() As Integer
+        Dim arrAverageScore() As Integer
+        Dim arrMax() As Integer
+        Dim arrpos() As Integer
+        Sub MethodA()
+            ReDim arrMax(55)
+            ReDim arrpos(55)
+            Dim sum As Integer = 0
+            Dim temp As Integer
+            Dim temp2 As Integer
+            For i = 1 To 55
+                If frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(i) = 0 Then
+                    sum = 0
+                    For x As Integer = 1 To frmBookRec.TotalReaders
+                        sum += frmBookRec.Readers.arrReaders(x).arrRatings(i)
+                    Next
+                    sum = sum / frmBookRec.TotalReaders
+                    arrMax(i) = sum
+                    arrpos(i) = i
+                End If
+            Next
+            For x = 1 To 55
+                For y = 2 To 55
+                    If arrMax(x) > arrMax(y) Then
+                        temp = arrMax(y)
+                        temp2 = arrpos(y)
+                        arrMax(y) = arrMax(x)
+                        arrpos(y) = arrpos(x)
+                        arrMax(x) = temp
+                        arrpos(x) = temp
+                    End If
+                Next y
+            Next x
+            For i = 2 To 6
+                frmBookRec.txtRec.Text = frmBookRec.Books.arrBooks(arrpos(i)).Title
+                frmBookRec.lstBooks.Items.Add(frmBookRec.Books.arrBooks(arrpos(i)).Title)
+            Next
+        End Sub
+        Sub MethodB()
+            ReDim arrSimilarityScore(frmBookRec.TotalReaders)
+            For i As Integer = 1 To frmBookRec.TotalReaders
+                If i <> frmBookRec.ReaderPos Then
+                    Dim SScore As Integer = 0
+                    For x As Integer = 1 To 55
+                        If frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(x) <> 6 Then
+                            SScore = SScore + (frmBookRec.Readers.arrReaders(i).arrRatings(x) * frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(x))
+                        End If
+                    Next
+                    arrSimilarityScore(i) = SScore
+                End If
+            Next
+            Dim maxpos As Integer = 0
+            Dim max As Integer = -1375
+            For i = 1 To frmBookRec.TotalReaders
+                If arrSimilarityScore(i) > max Then
+                    max = arrSimilarityScore(i)
+                    maxpos = i
+                End If
+            Next
+            MsgBox("Highest Similarity Score Was: " & max)
+            Dim OutputString As String = ""
+            MsgBox("Most Similar Reader Was: " & frmBookRec.Readers.arrReaders(maxpos).Name)
+            For x = 1 To 55
+                If frmBookRec.Readers.arrReaders((frmBookRec.ReaderPos)).arrRatings(x) = 0 Then
+                    If frmBookRec.Readers.arrReaders(maxpos).arrRatings(x) >= 3 Then
+                        OutputString = OutputString & frmBookRec.Books.arrBooks(x).Title & "," & frmBookRec.Books.arrBooks(x).Author & "   "
+                        frmBookRec.lstBooks.Items.Add(frmBookRec.Books.arrBooks(x).Title)
+                    End If
+                End If
+            Next
+            frmBookRec.txtRec.Text = OutputString
+        End Sub
+        Sub MethodC()
+            ReDim arrSimilarityScore(frmBookRec.TotalReaders)
+            For i As Integer = 1 To frmBookRec.TotalReaders
+                If i <> frmBookRec.ReaderPos Then
+                    Dim SScore As Integer = 0
+                    For x As Integer = 1 To 55
+                        If frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(x) <> 6 Then
+                            SScore = SScore + (frmBookRec.Readers.arrReaders(i).arrRatings(x) * frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(x))
+                        End If
+                    Next
+                    arrSimilarityScore(i) = SScore
+                End If
+            Next
+            ReDim arrAverageScore(55)
+            Dim max As Integer = 0
+            Dim maxpos As Integer = 0
+            Dim OutputString As String = ""
+            For i As Integer = 1 To 55
+                Dim AScore As Integer = 0
+                For x As Integer = 1 To frmBookRec.TotalReaders
+                    AScore = AScore + (frmBookRec.Readers.arrReaders(x).arrRatings(i) * arrSimilarityScore(x))
+                Next
+                arrAverageScore(i) = AScore
+            Next
+            For i = 1 To 55
+                If arrAverageScore(i) > max And frmBookRec.Readers.arrReaders(frmBookRec.ReaderPos).arrRatings(i) = 0 Then
+                    max = arrAverageScore(i)
+                    maxpos = i
+                End If
+            Next
+            OutputString = frmBookRec.Books.arrBooks(maxpos).Title & "," & frmBookRec.Books.arrBooks(maxpos).Author & " "
+            frmBookRec.txtRec.Text = OutputString
+            frmBookRec.lstBooks.Items.Add(frmBookRec.Books.arrBooks(maxpos).Title)
+        End Sub
+    End Structure
+    Dim Recommendations As BookRec
+    Private Sub lstReaders_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstReaders.SelectedIndexChanged
+        ReaderPos = lstReaders.SelectedIndex + 1
+        lblReader.Text = Readers.arrReaders(ReaderPos).Name
+    End Sub
+    Dim ReaderPos As Integer
+    Dim BookPos As Integer
+    Private Sub cmbEdit_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbEdit.SelectedIndexChanged
+        If cmbEdit.SelectedIndex = 0 Then 'Add
+            Dim name As String = InputBox("Enter Your Name:", "New Reader")
+            Readers.Add(name)
+            lstReaders.Items.Clear()
+            For i = 1 To (Readers.arrReaders.Length) - 1
+                lstReaders.Items.Add(Readers.arrReaders(i).Name)
+            Next
+            ReaderPos = Readers.arrReaders.Length - 1
+            lblReader.Text = Readers.arrReaders(ReaderPos).Name
+            BookPos = 0
+            lblAuthor.Text = Books.arrBooks(BookPos + 1).Author
+            lblTitle.Text = Books.arrBooks(BookPos + 1).Title
+            ReDim Readers.arrReaders(ReaderPos).arrRatings(55)
+        ElseIf cmbEdit.SelectedIndex = 1 Then 'Change
+            ReaderPos = lstReaders.SelectedIndex + 1
+            lblReader.Text = Readers.arrReaders(ReaderPos).Name
+            lblAuthor.Text = Books.arrBooks(1).Author
+            lblTitle.Text = Books.arrBooks(1).Title
+            Dim Rating As Integer = Readers.arrReaders(ReaderPos).arrRatings(1)
+            If Rating = -5 Then
+                arrButtons(1).Checked = True
+            ElseIf Rating = -3 Then
+                arrButtons(2).Checked = True
+            ElseIf Rating = 0 Then
+                arrButtons(3).Checked = True
+            ElseIf Rating = 1 Then
+                arrButtons(4).Checked = True
+            ElseIf Rating = 3 Then
+                arrButtons(5).Checked = True
+            ElseIf Rating = 5 Then
+                arrButtons(6).Checked = True
+            End If
+            BookPos = 1
+        ElseIf cmbEdit.SelectedIndex = 2 Then 'Delete
+            TotalReaders = TotalReaders - 1
+            For i = ReaderPos To TotalReaders
+                Readers.arrReaders(i).Name = Readers.arrReaders(i + 1).Name
+                Readers.arrReaders(i).arrRatings = Readers.arrReaders(i + 1).arrRatings
+            Next
+            ReDim Preserve Readers.arrReaders(TotalReaders)
+            lstReaders.Items.Clear()
+            For i = 1 To TotalReaders
+                lstReaders.Items.Add(Readers.arrReaders(i).Name)
+            Next
+        End If
+    End Sub
+
+    Private Sub cmdNext_Click(sender As Object, e As EventArgs) Handles cmdNext.Click
+        If BookPos + 1 >= 56 Then
+            MsgBox("You've Rated All The Books!")
+        Else
+            BookPos = BookPos + 1
+        End If
+        lblTitle.Text = Books.arrBooks(BookPos).Title
+        lblAuthor.Text = Books.arrBooks(BookPos).Author
+        Dim Checked As Boolean = False
+        Dim i As Integer = 1
+        Dim ButtonPos As Integer
+        Do Until Checked = True
+            If arrButtons(i).Checked = True Then
+                Checked = True
+                ButtonPos = i
+            End If
+            i = i + 1
+        Loop
+        Readers.arrReaders(ReaderPos).arrRatings(BookPos) = Val(arrButtons(ButtonPos).Text)
+    End Sub
+
+    Private Sub CmbMethods_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbMethods.SelectedIndexChanged
+        If cmbMethods.SelectedIndex = 0 Then 'Method A
+            lstBooks.Items.Clear()
+            Recommendations.MethodA()
+        ElseIf cmbMethods.SelectedIndex = 1 Then 'Method B
+            lstBooks.Items.Clear()
+            Recommendations.MethodB()
+        ElseIf cmbMethods.SelectedIndex = 2 Then 'Method C
+            lstBooks.Items.Clear()
+            Recommendations.MethodC()
+        End If
+    End Sub
+
+    Private Sub cmdDone_Click(sender As Object, e As EventArgs) Handles cmdDone.Click
+        For i As Integer = BookPos + 1 To 55
+            Readers.arrReaders(ReaderPos).arrRatings(i) = 6
+        Next
+    End Sub
+
+    Private Sub lstBooks_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstBooks.SelectedIndexChanged
+        Dim title As String = lstBooks.SelectedItem
+        Dim path As String = "D:\CP\HCP2\Projects\BookRec\images\" & title & ".jpg"
+        picBookCover.Image = Image.FromFile(path)
+    End Sub
+    Private Sub frmBookRec_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Dim path As String = "D:\CP\HCP2\Projects\BookRec\readerratings.txt"
+        System.IO.File.WriteAllText(path, "")
+        FileOpen(1, path, OpenMode.Output)
+        For i As Integer = 1 To TotalReaders
+            PrintLine(1, Readers.arrReaders(i).Name)
+            Dim strout As String = ""
+            For x As Integer = 1 To 55
+                strout = strout & Trim(Str(Readers.arrReaders(i).arrRatings(x))) & " "
+            Next x
+            PrintLine(1, strout)
+        Next i
+    End Sub
+End Class
